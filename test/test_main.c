b3b98b012fcd1747b6fd415de5a1619340e34adf
OmarMesqq <omarmsqt@gmail.com>
2026-01-17 13:46:08 -0300

    start `get_basename` tests

--- test/test_main.c
+++ test/test_main.c
@@ -3,13 +3,30 @@
 #include <string.h>
 #include <stdlib.h>
 
-#define MAX_FILE_BASENAME 200
+#define MAX_FILE_BASENAME_LEN 200
+#define SMALL_MAX_FILE_BASENAME_LEN 5
 
 int main(void) {
-  char* fullFilePath = "/some/file/path/with/basename/at/end";
-  char* bn = get_basename(fullFilePath, MAX_FILE_BASENAME);
-  assert(strcmp(bn, "end") == 0);
+  /** get_basename tests */
+
+  char* filePath = NULL;
+  unsigned maxBaseNameLength = 0;
+  char* actualBaseName = NULL;
+
+  // 1. no filePath
+  assert(get_basename(filePath, maxBaseNameLength) == NULL);
+
+  // 2. no max length is zero
+  filePath = "/some/path";
+  maxBaseNameLength = 0;
+  assert(get_basename(filePath, maxBaseNameLength) == NULL);
+
+  // 3. path starting with /
+  filePath = "/i/start/with/a/slash";
+  maxBaseNameLength = MAX_FILE_BASENAME_LEN;
+  actualBaseName = get_basename(filePath, maxBaseNameLength);
+  assert(strcmp(actualBaseName, "slash") == 0);
+  free(actualBaseName);
   
-  free(bn);
   return 0;
 }
