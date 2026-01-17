69429ecaedf4fb2989d2d34e583b9800a5ef66c7
OmarMesqq <omarmsqt@gmail.com>
2026-01-17 13:53:11 -0300

    test: more basename tests

--- test/test_main.c
+++ test/test_main.c
@@ -22,11 +22,33 @@ int main(void) {
   assert(get_basename(filePath, maxBaseNameLength) == NULL);
 
   // 3. path starting with /
-  filePath = "/i/start/with/a/slash";
+  filePath = "/I/start/with/a/slash";
   maxBaseNameLength = MAX_FILE_BASENAME_LEN;
   actualBaseName = get_basename(filePath, maxBaseNameLength);
   assert(strcmp(actualBaseName, "slash") == 0);
   free(actualBaseName);
+
+  // 4. path NOT starting with /
+  filePath = "I/do/not/start/with/a/slash";
+  maxBaseNameLength = MAX_FILE_BASENAME_LEN;
+  actualBaseName = get_basename(filePath, maxBaseNameLength);
+  assert(strcmp(actualBaseName, "slash") == 0);
+  free(actualBaseName);
+
+  // 5. path  starting with .
+  filePath = "./I/start/with/a/dot";
+  maxBaseNameLength = MAX_FILE_BASENAME_LEN;
+  actualBaseName = get_basename(filePath, maxBaseNameLength);
+  assert(strcmp(actualBaseName, "dot") == 0);
+  free(actualBaseName);
+
+  // 6. path  starting with ..
+  filePath = "../I/start/with/..";
+  maxBaseNameLength = MAX_FILE_BASENAME_LEN;
+  actualBaseName = get_basename(filePath, maxBaseNameLength);
+  assert(strcmp(actualBaseName, "..") == 0);
+  free(actualBaseName);
   
+  printf("All Cutilities tests passed!\n");
   return 0;
 }
